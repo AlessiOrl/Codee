c33b1d5351bca4ad6da8548cf5bafc83442ca11e
AlessiOr <orlandoalessio1998@gmail.com>
2025-04-18 19:07:21 +0200

    Add LLM and embedding API integration, update chat history management, and enhance assistant prompt

--- app/main.py
+++ app/main.py
@@ -19,13 +19,16 @@ from datetime import datetime
 import json
 import logging
 import os
+import time
 import httpx as requests
 
 from telegram import ForceReply, Update, constants
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, Updater, CallbackContext, MessageHandler
 import os
+from utils import get_prompt, update_chat_history, calculate_similarity
 
-WHISPERAPI_URL = os.environ['WHISPERAPI_URL']
+LLM_URL = os.environ['LLM_URL']
+ENCODER_URL = os.environ['ENCODER_URL']
 BOT_TOKEN = os.environ['BOT_TOKEN']
 
 # Enable logging
@@ -37,16 +40,35 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 from datetime import datetime, timezone
 
+
+def call_embedder_api(prompt, messages):
+    return [0.2, 0.3, 0.4]  # Placeholder for actual embedding call
+
+def call_llm_api(system_prompt, messages):
+    url = "http://192.168.178.200:8000/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "messages": [
+            { "role": "system", "content": system_prompt },
+        ],
+        "cache_prompt": True,
+        "temperature": 0.4,
+        "top_k": 1,
+        "stream": True,
+        "repeat_penalty": 2,
+    }
+    for message in messages:
+        payload["messages"].append({
+            "role": message["role"],
+            "content": message["content"]
+        })
+
+    return requests.post(url, headers=headers, json=payload, stream=True) 
+
 def utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
-
-def call_whisper_api(chatID: str, filename: str) -> dict:
-    """Call the whisper API with the given endpoint and data."""    
-    # send the voice note to the whisper API
-    path = f"{WHISPERAPI_URL}/queue/{chatID}"
-    files = {'audio_file':  open(f"data/{filename}", 'rb')}
-    res = requests.post(path, files=files)
-    return res
 
 async def start_handler(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
@@ -62,241 +84,71 @@ async def help_handler(update: Update, context: CallbackContext) -> None:
         text="Send me an audio message and I'll add it to the queue. Use /speech2text_queue to get the current queue, /speech2text_start to start the speech2text process, /speech2text_clear to clear the queue, /speech2text_info to get information about the process, and /speech2text_help to get help information.",
     )
 
-async def audio_message_handler(update: Update, context: CallbackContext) -> None:
-    """Send the user a message that the bot received the audio message. And that it has been added to the queue."""
-    # get chat ID from the message
-    chatID = update.effective_chat.id
-
-    try:
-        first_name = update.message.api_kwargs["forward_from"]["first_name"]
-    except Exception as e:
-        first_name = update.message.chat.first_name
-    date = utc_to_local(update.message.date).strftime("%H-%M")
-    duration = update.message.audio.duration
-    new_file = await context.bot.get_file(update.message.audio.file_id)
-    filename = f"{first_name}-{duration}_{date}.mp3"
-
-    # download the audio note as a file
-    await new_file.download_to_drive(f"data/{filename}")
-
-    # handle the response from the whisper API
-    response = call_whisper_api(chatID, filename)
-    if response.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Your audio note has been added to the queue.",
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error adding your audio note to the queue. Please try again later.",
-        )
-    
-    # remove the audio note file
-    os.remove(f"data/{filename}")
-
 async def codee_llm_handler(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     chatID = update.effective_chat.id
     message = update.message.text
     complete_text = ""
-    with requests.stream('GET', f"{WHISPERAPI_URL}/predict_llm/{chatID}", json={"message": message}, timeout=30) as r:
+
+    prompt, messages = get_prompt(chatID, message, message)
+
+    interaction = []
+    interaction_timsetamp = time.time() 
+    
+    embedding = call_embedder_api(messages)
+
+    interaction.append({
+            "chat_id": chatID,
+            "role": "user",
+            "content": embedding,
+            "timestamp": interaction_timsetamp,
+            "embedding": embedding,
+            "metadata": None,
+        })
+
+    messages = [{"role": "user", "content": embedding}]
+    
+    llm_response = call_llm_api(prompt, messages)
+    code = llm_response.status_code
+    if code == 200:
+        i = 0
+        aux_last_message = ""
         sentence_chunk = ""
-        code = r.status_code
-        if code == 200:
-            i = 0
-            for chunk in r.iter_raw():
-                decoded = chunk.decode()
-                if any(x in decoded for x in ["<|eot_id|>", "<|im_end|>", "<|eot|>", "</s>"]):
-                    decoded = decoded.replace("<|eot_id|>", "")
-                    decoded = decoded.replace("<|im_end|>", "")
-                    decoded = decoded.replace("<|eot|>", "")
-                    decoded = decoded.replace("</s>", "")
-                sentence_chunk += decoded
-                # if the sentence_chunk has a period, exclamation mark, question mark, semi-colon or colon, then it is a complete sentence
-                if any(x in sentence_chunk for x in [".", "!", "?", ";", ":"]):
-                    complete_text += sentence_chunk
-                    sentence_chunk = ""
-                    if len(complete_text) > 0:
-                        if  i == 0:
-                            # if is the first chunk start the response message 
-                            response_msg = await context.bot.send_message(chat_id=update.effective_chat.id, text=complete_text)
-                            aux_last_message = complete_text
-                        else:
-                            # if is not the first chunk edit the response message
-                            await context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=response_msg.message_id, text=complete_text)
-                            aux_last_message = complete_text
-                        i += 1
-            complete_text += sentence_chunk
-            if i > 0 and aux_last_message != complete_text:
-                # send the final response message
-                await context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=response_msg.message_id, text=complete_text)
-            requests.post(f"{WHISPERAPI_URL}/predicted_text/{chatID}", json={"message": complete_text})
-        elif code == 401:
-            for chunk in r.iter_raw():
-                decoded = chunk.decode()
-                complete_text += decoded
-            complete_text = json.loads(complete_text)["detail"]
-            await context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=complete_text,
-            )
-        else:
-            await context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text="There was an error getting the response from the model. Please try again later.",
-            )
-
-async def voice_message_handler(update: Update, context: CallbackContext) -> None:
-    """Send the user a message that the bot received the voice message. And that it has been added to the queue."""
-    # get chat ID from the message
-    chatID = update.effective_chat.id
-    try:
-        first_name = update.message.api_kwargs["forward_from"]["first_name"]
-    except Exception as e:
-        first_name = update.message.chat.first_name
-    date = utc_to_local(update.message.date).strftime("%H-%M")
-    duration = update.message.voice.duration
-    new_file = await context.bot.get_file(update.message.voice.file_id)
-    filename = f"{first_name}-{duration}_{date}.ogg"
-
-    # download the audio note as a file
-    await new_file.download_to_drive(f"data/{filename}")
-
-    # handle the response from the whisper API
-    response = call_whisper_api(chatID, filename)
-
-    if response.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Your audio note has been added to the queue.",
-        )
+        for response_orig in llm_response.iter_lines():
+            response = response_orig.decode("utf-8").replace("data: ", "")
+            if response == "[DONE]":
+                break
+            if response == "":
+                continue
+            response = json.loads(response)
+            if "choices" in response:
+                for choice in response["choices"]:
+                    if "delta" in choice and "content" in choice["delta"]:
+                        sentence_chunk += choice["delta"]["content"]
+                        # if the sentence_chunk has a period, exclamation mark, question mark, semi-colon or colon, then it is a complete sentence
+                        if any(x in sentence_chunk for x in [".", "\n"]):
+                            complete_text += sentence_chunk
+                            sentence_chunk = ""
+                            if len(complete_text) > 0:
+                                if  i == 0:
+                                    # if is the first chunk start the response message 
+                                    response_msg = await context.bot.send_message(chat_id=update.effective_chat.id, text=complete_text)
+                                    aux_last_message = complete_text
+                                else:
+                                    # if is not the first chunk edit the response message
+                                    await context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=response_msg.message_id, text=complete_text)
+                                    aux_last_message = complete_text
+                                i += 1
+                    if "finish_reason" in choice and choice["finish_reason"] == "stop":
+                        break
+        complete_text += sentence_chunk
+        if i > 0 and aux_last_message != complete_text:
+            # send the final response message
+            await context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=response_msg.message_id, text=complete_text)
     else:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="There was an error adding your audio note to the queue. Please try again later.",
-        )
-    
-    # remove the audio note file
-    os.remove(f"data/{filename}")   
-
-async def video_note_message_handler(update: Update, context: CallbackContext) -> None:
-    """Send the user a message that the bot received the video message. And that it has been added to the queue."""
-    # get chat ID from the message
-    chatID = update.effective_chat.id
-
-    try:
-        first_name = update.message.api_kwargs["forward_from"]["first_name"]
-    except Exception as e:
-        first_name = update.message.chat.first_name
-    date = utc_to_local(update.message.date).strftime("%H-%M")
-    duration = update.message.video_note.duration
-    
-    # create filename for the video note with a timestamp
-    filename = f"{first_name}-{duration}_{date}"
-
-    # get basic info about the video note file and prepare it for downloading
-    new_file = await context.bot.get_file(update.message.video_note.file_id)
-    # download the video note as a file
-    await new_file.download_to_drive(f"data/{filename}.mp4")
-
-    # convert it to an audio file
-    os.system(f"ffmpeg -v quiet -i {filename}.mp4 -q:a 0 -map a {filename}.mp3")
-    # remove the video note file
-    os.remove(f"data/{filename}.mp4")
-    # handle the response from the whisper API
-    response = call_whisper_api(chatID, f"{filename}.mp3")
-    if response.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Your video note has been added to the queue.",
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error adding your video note to the queue. Please try again later.",
-        )
-
-    # remove the audio note file
-    os.remove(f"data/{filename}.mp3")
-
-async def get_speech2text_queue(update: Update, context: CallbackContext) -> None:
-    """Get the current speech2text queue."""
-    res = requests.get(f"{WHISPERAPI_URL}/queue/{update.effective_chat.id}")
-    if res.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=json.loads(res.text),
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error getting the speech2text queue. Please try again later.",
-        )
-
-async def start_speech2text(update: Update, context: CallbackContext) -> None:
-    """Start the speech2text process."""
-    chatID = update.effective_chat.id
-    await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Starting the speech2text process. Wait a moment...",
-        )
-    res = requests.get(f"{WHISPERAPI_URL}/predict_whisper/{chatID}?remove=True")
-    print(res)
-    if res.status_code == 200:
-        for key, value in json.loads(res.text).items():
-            await context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                parse_mode=constants.ParseMode.HTML,
-                text=f"<b>{key}</b>:\n{value.strip()}",
-            )
-    else:
-        print(res.text)
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error starting the speech2text process. Please try again later.",
-        )
-    
-async def clear_speech2text(update: Update, context: CallbackContext) -> None:
-    """Clear the speech2text queue."""
-    res = requests.get(f"{WHISPERAPI_URL}/clear_queue/{update.effective_chat.id}")
-    if res.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="The speech2text queue has been cleared.",
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error clearing the speech2text queue. Please try again later.",
-        )
-
-async def info_speech2text(update: Update, context: CallbackContext) -> None:
-    """Get information about the speech2text process."""
-    res = requests.get(f"{WHISPERAPI_URL}/info")
-    if res.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=json.loads(res.text),
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error getting the speech2text information. Please try again later.",
-        )
-
-async def help_speech2text(update: Update, context: CallbackContext) -> None:
-    """Get help information about the speech2text process."""
-    res = requests.get(f"{WHISPERAPI_URL}/help")
-    if res.status_code == 200:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=json.loads(res.text),
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="There was an error getting the speech2text information. Please try again later.",
+            text="There was an error getting the response from the model. Please try again later.",
         )
 
 def main() -> None:
@@ -311,19 +163,6 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start_handler))
     application.add_handler(CommandHandler("help", help_handler))
 
-    # Add voice message handler
-    application.add_handler(MessageHandler(filters.VOICE, voice_message_handler))
-    application.add_handler(MessageHandler(filters.AUDIO, audio_message_handler))
-
-    # add video message handler
-    application.add_handler(MessageHandler(filters.VIDEO_NOTE, video_note_message_handler))
-
-    # add speech2text handlers
-    application.add_handler(CommandHandler("speech2text_queue", get_speech2text_queue))
-    application.add_handler(CommandHandler("speech2text_start", start_speech2text))
-    application.add_handler(CommandHandler("speech2text_clear", clear_speech2text))
-    application.add_handler(CommandHandler("speech2text_info", info_speech2text))
-    application.add_handler(CommandHandler("speech2text_help", help_speech2text))
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
---
+++ app/utils.py
@@ -0,0 +1,109 @@
+import os, pymongo, json, time
+
+DB_URL = os.environ['DB_URL']
+
+def calculate_similarity(message1, message2):
+    # Placeholder for actual similarity calculation
+    # For now, we will just return a dummy value
+    return 0.2
+
+def update_chat_history(chat_id, message, response):
+    try:
+        timsetamp = time.time()
+        mongodb_conn = pymongo.MongoClient(f"mongodb://{DB_URL}/")
+        
+        # insert the interaction into the database
+        llm_user = mongodb_conn[f"User_{chat_id}"]
+        llm_chathistory = llm_user[f"ChatHistory"]
+
+        interaction = []
+        row = {
+            "chat_id": chat_id,
+            "role": message["role"],
+            "content": message["content"],
+            "timestamp": timsetamp,
+            "embedding": message["embedding"],
+            "metadata": None,
+        }
+        interaction.append(row)
+        row = {
+            "chat_id": chat_id,
+            "role": response["role"],
+            "content": response["content"],
+            "timestamp": timsetamp,
+            "embedding": response["embedding"],
+            "metadata": None,
+        }
+        interaction.append(row)
+
+        # insert the interaction into the database
+        llm_chathistory.insert_many(interaction)
+        # close the connection
+        mongodb_conn.close()
+    except Exception as e:
+        print(f"Error updating chat history: {e}")
+        return False
+    return True
+
+def get_chat_history(chat_id):
+    mongodb_conn = pymongo.MongoClient(f"mongodb://{DB_URL}/")
+    llm_user = mongodb_conn[f"User_{chat_id}"]
+    llm_chathistory = llm_user[f"ChatHistory"]
+
+    return llm_chathistory, mongodb_conn
+
+def get_prompt(chat_id, completion, user_message):
+    # connect to the database
+    llm_chathistory, mongodb_conn = get_chat_history(chat_id)
+
+    # load prompt
+    with open("./data/codee_assistant_prompt.txt", "r", encoding="utf-8") as f:
+        prompt = f.read()
+
+    # add <CONTEXT_HISTORY> to the prompt
+    query_context = {"chat_id": chat_id}
+    recent_100_messages = llm_chathistory.find(query_context).sort("timestamp", pymongo.DESCENDING).limit(100)
+    similairies = []
+    for message in recent_100_messages:
+        similairy = calculate_similarity(message["content"], user_message)
+        # filter out similarities under a certain threshold
+        if similairy > 0.2:
+            similairies.append({"role": message["role"], "content": message["content"], "similarity": similairy})
+    # sort the messages by similarity
+    similairies.sort(reverse=True)
+    # get the top 5 most similar messages
+    top_5_similar_messages = similairies[:5]
+
+    # add the top 5 messages to the prompt
+    meta_prompt = ""
+    for message in top_5_similar_messages:
+        meta_prompt += message["role"] + ": " + message["content"] + "\n"
+
+    if meta_prompt == "":
+        meta_prompt = "No similar messages found.\n"
+
+    # add the meta prompt to the prompt
+    prompt = prompt.replace("<CONTEXT_HISTORY>", meta_prompt)
+
+    # add <TEMPORAL_HISTORY> to the prompt
+    # get the last 10 messages 
+    query = {"chat_id": chat_id}
+    recent_messages = llm_chathistory.find(query).sort("timestamp", pymongo.DESCENDING).limit(10)
+    # add the messages to the prompt
+    meta_prompt = ""
+    for message in recent_messages:
+        meta_prompt += message["role"] + ": " + message["content"] + "\n"
+
+    if meta_prompt == "":
+        meta_prompt = "No recent messages found.\n"
+
+    # add the meta prompt to the prompt
+    prompt = prompt.replace("<TEMPORAL_HISTORY>", meta_prompt)
+
+    messages = [{"role": "system", "content": prompt}, {"role": "user", "content": completion}]
+    
+    # close the connection
+    mongodb_conn.close()
+
+    return prompt, messages
+
